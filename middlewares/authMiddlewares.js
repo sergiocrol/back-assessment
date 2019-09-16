f116a0774ffc7422406547457e2433a8076c0796
Sergio Cordero <sergio.crol@gmail.com>
2019-09-16 21:08:29 +0200

    Auth token created

--- middlewares/authMiddlewares.js
+++ middlewares/authMiddlewares.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const jwt = require('jsonwebtoken');
+
 const isLoggedIn = (req, res, next) => {
   if (req.session.currentUser) {
     return res.redirect('/');
@@ -9,11 +11,18 @@ const isLoggedIn = (req, res, next) => {
 
 // Verify if the user is logged in
 const verifyToken = (req, res, next) => {
-  const bearerHeader = req.headers.authorization;
+  const bearerHeader = req.headers['x-access-token'] || req.headers.authorization;
+
   // Check if token has been sent
   if (typeof bearerHeader !== 'undefined') {
     req.token = bearerHeader.split(' ')[1];
-    next();
+    jwt.verify(req.token, process.env.JWT_MY_SECRET, async (error) => {
+      if (error) {
+        res.status(401).send({ success: false, unauthorized: 'You have to login to access to this info' });
+      } else {
+        next();
+      }
+    });
   } else {
     res.status(401).send({ success: false, unauthorized: 'You have to login to access to this info' });
   }
@@ -31,11 +40,11 @@ const isNotLoggedIn = (req, res, next) => {
   next();
 };
 
-const validationLoggin = (req, res, next) => {
+const validationLogin = (req, res, next) => {
   const { email } = req.body;
 
   if (!email) {
-    res.status(422).json({ '422 - Unprocessable Entity': 'Unable to process the received data' });
+    res.status(422).send({ 'Unprocessable Entity': 'Unable to process the received data' });
   } else {
     next();
   }
@@ -45,6 +54,6 @@ module.exports = {
   isLoggedIn,
   isNotLoggedIn,
   verifyToken,
-  validationLoggin,
+  validationLogin,
   isAdmin
 };
--- routes/index.js
+++ routes/index.js
@@ -6,43 +6,36 @@ const jwt = require('jsonwebtoken');
 
 const APIhelper = require('../helpers/APIhelper');
 
-const { isNotLoggedIn, verifyToken, validationLoggin, isAdmin } = require('../middlewares/authMiddlewares');
+const { isNotLoggedIn, verifyToken, validationLogin, isAdmin } = require('../middlewares/authMiddlewares');
 
-router.get('/', /* isNotLoggedIn, */(req, res, next) => {
+router.get('/', (req, res, next) => {
   res.json({
-    message: 'Welcome to INSURAN.CE API :)'
+    'Get users by ID': 'http://localhost:3000/api/users/id',
+    'Get users by name': 'http://localhost:3000/api/users/name',
+    'Get user linked to policy': 'http://localhost:3000/api/users/policy',
+    'Get policies of user': 'http://localhost:3000/api/policies'
   });
 });
 
-router.get('/users/id', verifyToken, (req, res) => {
-  // Check if there is an existing token and get user data by username passed in the body
+router.get('/users/id', verifyToken, async (req, res) => {
+  // Check if there is an existing token and get the user's data by the username passed in the body
   const { id } = req.body;
-  jwt.verify(req.token, process.env.JWT_MY_SECRET, async (error) => {
-    if (error) {
-      res.status(401).send({ success: false, unauthorized: 'You have to login to access to this info' });
-    } else {
-      let user = await APIhelper.getUsers();
-      user = user.filter(us => us.id === id);
-      res.json({
-        user
-      });
-    }
+
+  let user = await APIhelper.getUsers();
+  user = user.filter(us => us.id === id);
+  res.json({
+    user
   });
 });
 
-router.get('/users/name', verifyToken, (req, res) => {
+router.get('/users/name', verifyToken, async (req, res) => {
   // Check if there is an existing token and get user data by id passed in the body
   const { name } = req.body;
-  jwt.verify(req.token, process.env.JWT_MY_SECRET, async (error) => {
-    if (error) {
-      res.status(401).send({ success: false, unauthorized: 'You have to login to access to this info' });
-    } else {
-      let user = await APIhelper.getUsers();
-      user = user.filter(user => user.name === name);
-      res.json({
-        user
-      });
-    }
+
+  let user = await APIhelper.getUsers();
+  user = user.filter(user => user.name === name);
+  res.json({
+    user
   });
 });
 
@@ -50,13 +43,27 @@ router.get('/policies', verifyToken, isAdmin, (req, res) => {
 
 });
 
-router.post('/login', /* isNotLoggedIn, validationLoggin, */(req, res, next) => {
+router.post('/login', /* isNotLoggedIn, */ validationLogin, async (req, res, next) => {
   // check if the email is in the data received from external API. If it is, save token in the local storage
   const { email } = req.body;
 
-  jwt.sign({ email }, process.env.JWT_MY_SECRET, (err, token) => {
-    res.json({ token });
-  });
+  let user = await APIhelper.getUsers();
+  user = user.filter(user => user.email === email);
+
+  if (user.length !== 0) {
+    const token = jwt.sign({ email }, process.env.JWT_MY_SECRET, { expiresIn: '24h' });
+
+    res.status(200).send({
+      success: true,
+      message: 'Authentication successful!',
+      token
+    });
+  } else {
+    res.status(400).send({
+      success: false,
+      message: 'Authentication failed! This email is not in the database'
+    });
+  }
 });
 
 module.exports = router;
